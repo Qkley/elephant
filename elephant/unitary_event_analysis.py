c484b46096175fc46df37a7abffbba2aaae8bc38
Michael Denker <m.denker@fz-juelich.de>
2019-06-06 13:22:25 +0200

    Undid a wrong fix from merge in UE function

--- elephant/unitary_event_analysis.py
+++ elephant/unitary_event_analysis.py
@@ -829,7 +829,7 @@ def unitary_event_analysis(
     mat_tr_unit_spt = np.zeros((len(data), N, n_bins))
     for tr, sts in enumerate(data):
         sts = list(sts)
-        bs = conv.BinnedSpikeTrain(
+        bs = BinnedSpikeTrain(
             sts, t_start=t_start, t_stop=t_stop, binsize=binsize)
         if binary is True:
             mat = bs.to_bool_array()
